4bcf9cfbe030136b503c7edeb87437e1620d31c0
BBlyde <benjaminblondey@orange.fr>
2023-06-02 10:50:44 +0200

    Final commit I believe

--- app/src/main/java/com/example/finalapp/About.java
+++ app/src/main/java/com/example/finalapp/About.java
@@ -3,6 +3,8 @@ package com.example.finalapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        /* Return button */
+        Button buttonBack = findViewById(R.id.buttonBackAbout);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
